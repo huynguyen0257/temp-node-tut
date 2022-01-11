d3e954476419e0e09d33335f19ce574350dc3fcc
Nguyen Gia Huy <huynguyen0257@gmail.com>
2022-01-11 21:29:15 +0700

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+const { huy, minh } = require("./4-name");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-flavor");
+require("./7-mind-grenade");
+
+// console.log(data);
+// sayHi(huy);
+// sayHi(minh);
---
+++ 4-name.js
@@ -0,0 +1,6 @@
+const secret = "SUPER SECRET";
+
+const huy = "huy";
+const minh = "minh";
+
+module.exports = { huy, minh };
